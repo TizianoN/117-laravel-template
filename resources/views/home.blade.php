514bfe1d6b4eb110572d2fc4f2f916b5740594f5
TizianoN <hello@tizianonicolai.com>
2024-03-19 10:27:08 +0100

    add layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-  <head>
-    <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="ie=edge" http-equiv="X-UA-Compatible">
-    <title>Document</title>
+@section('title', 'Pagina iniziale')
 
-    @vite('resources/js/app.js')
-  </head>
-
-  <body>
-    <h1>Laravel with Bootstrap + Vite</h1>
-  </body>
-
-</html>
+@section('main-content')
+  <section>
+    <div class="container py-4">
+      <h1>Laravel with Bootstrap + Vite</h1>
+    </div>
+  </section>
+@endsection
